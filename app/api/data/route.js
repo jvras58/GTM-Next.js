8b0d2043db414e1115030c0367a05a325763f823
jvras <jvras@cin.ufpe.br>
2024-10-22 16:56:39 -0300

    Refactor GTM data handling and routing for cleaner code and improved readability

---
+++ app/api/data/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(req) {
+try {
+const data = await req.json();
+console.log('Dados recebidos do GTM:', data);
+
+return NextResponse.json({ message: 'Dados recebidos com sucesso' }, { status: 200 });
+} catch (error) {
+console.error('Erro ao processar a requisição:', error);
+return NextResponse.json({ error: 'Ocorreu um erro interno do servidor.' }, { status: 500 });
+}
+}
+
+export async function GET() {
+return NextResponse.json({ message: 'Método HTTP não permitido' }, { status: 405 });
+}
