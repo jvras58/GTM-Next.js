3cade79ea416e4de224cbb421f1794b7f2f381a9
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-11-13 19:53:31 -0300

    feat: Refatorar componente BetIframe para usar Card e Button, adicionando redirecionamento ao clicar

--- components/BetIframe.jsx
+++ components/BetIframe.jsx
@@ -1,23 +1,37 @@
-
-
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const BetCard = ({ name, image, dbParam }) => {
+const router = useRouter();
+
+const handleRedirect = () => {
+router.push(`/bets/${dbParam}`);
+};
+
 return (
-<div className="bg-white shadow-lg rounded-lg overflow-hidden">
-    <img src={image} alt={name} className="w-full h-32 object-cover" />
-    <div className="p-4">
-    <h2 className="text-lg font-semibold">{name}</h2>
-    <p className="text-sm text-gray-500">Afiliado: {dbParam}</p>
-    
-    <Link href={`/bets/${dbParam}`}>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Acessar
-        </button>
-    </Link>
-    </div>
+<div className="container mx-auto mt-4">
+    <Card className="max-w-sm mx-auto mt-4">
+    <CardHeader>
+        <CardTitle>{name}</CardTitle>
+    </CardHeader>
+    <CardContent>
+        <Image
+        src={image}
+        alt={name}
+        className="w-full h-32 object-cover"
+        width={500}
+        height={200}
+        priority={false}
+        />
+    </CardContent>
+    <CardFooter className="flex justify-center">
+        <Button id='Acessar-click' onClick={handleRedirect}>Acessar</Button>
+    </CardFooter>
+    </Card>
 </div>
 );
 };
